5137fe359b060231591bcd8a72f708ec71d65745
Platun0v <platun0v@protonmail.com>
2020-10-23 13:18:36 +0300

    Base for bot

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ import os
 load_dotenv(".env")
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+USERS = {
+    447411595: "Коля",
+}
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import telebot
 import config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message: telebot.types.Message):
+    bot.send_message(message.chat.id, message.from_user.id)
+
+
+bot.polling()
