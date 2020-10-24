c70a8f61c35cfae56aef45966b95d46127d0664a
Platun0v <platun0v@protonmail.com>
2020-10-24 23:44:54 +0300

    Add reading of db

--- db_connector.py
+++ db_connector.py
@@ -1,6 +1,9 @@
 import json
 import time
+import os
 from typing import Dict, List, Optional
+
+import config
 
 blocker: bool = False
 
@@ -16,15 +19,63 @@ class Credit:
         self.returned: bool = returned
         self.return_date: str = return_date
 
+    @classmethod
+    def from_json(cls, _json) -> "Credit":
+        return Credit(_json["amount"], _json["date"], _json["text_info"], _json["returned"], _json["return_date"])
+
+    def to_json(self):
+        return {"amount": self.amount, "date": self.date, "text_info": self.text_info, "returned": self.returned, "return_date": self.return_date}
+
 
 class DB:
     def __init__(self, path: str):
         self.path: str = path
-        self.db: Optional[Dict[Dict[List[Credit]]]] = None
+        self.db: Optional[Dict[int, Dict[int, List[Credit]]]] = None
 
     def read_file(self):
-        with open(self.path, 'r') as f:
-            json.load(f)
+        db_object = self.check_file()
+
+        for user in db_object.keys():
+            for inner_user in db_object[user].keys():
+                for i in range(len(db_object[user][inner_user])):
+                    db_object[user][inner_user][i] = Credit.from_json(db_object[user][inner_user][i])
+
+        self.db = db_object
+
+    def check_file(self) -> Dict[int, Dict[int, List]]:
+        if not os.path.exists(self.path):
+            write_json(self.create_db_object_frame(), self.path)
+
+        db_object: Dict[int, Dict[int, List]] = read_json(self.path)
+
+        # TODO: Processing new users
+        # db_keys = set(db_object.keys())
+        # users = set(config.USERS.keys())
+        # if db_keys != users:
+        #     new_users = users - db_keys
+
+        return db_object
+
+    @staticmethod  # Изначально есть дикт диктов с пустым массивом
+    def create_db_object_frame() -> Dict[int, Dict[int, List]]:
+        ids = set(config.USERS.keys())
+        db_object = {}
+        for user in ids:
+            db_object[user] = {}
+            for inner_user in ids - user:
+                db_object[user][inner_user] = []
+
+        return db_object
+
+
+def read_json(path):
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+def write_json(obj, path):
+    with open(path, "w") as f:
+        json.dump(obj, f)
 
 
 def block_file():
