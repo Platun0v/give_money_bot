54916a2190d9ad0d3941e84ee75f04ec174d6d10
Platun0v <platun0v@protonmail.com>
2020-11-18 14:51:18 +0300

    Finish refactoring

--- keyboards.py
+++ keyboards.py
@@ -1,44 +1,45 @@
 from typing import Set, Tuple, List, Union
 
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, \
-    InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.callback_data import CallbackData
+
 from config import USERS
 from config import Emoji
 from db_connector import Credit
-from aiogram.utils.callback_data import CallbackData
+from callback_data import CALLBACK
 
-credit_amount_data = CallbackData("save", "value")
-user_data = CallbackData("user", "id", "has_mark")
-credit_data = CallbackData("credit_chose", "index", "has_mark")
-check_data = CallbackData("check", "credit_id", "value")
+credit_amount_data = CallbackData(CALLBACK.save_credit, "value")
+user_choose_data = CallbackData(CALLBACK.choose_user_for_credit, "id", "has_mark")
+credit_choose_data = CallbackData(CALLBACK.choose_credit_for_return, "index", "has_mark")
+check_return_data = CallbackData(CALLBACK.check_return_of_credit, "credit_id", "value")
 
-main_markup = ReplyKeyboardMarkup(resize_keyboard=True).row(KeyboardButton("+"), KeyboardButton("info"))
+main_markup = ReplyKeyboardMarkup(resize_keyboard=True).row(
+    KeyboardButton("+"),
+    KeyboardButton("-"),
+    KeyboardButton("info")
+)
 
-credits_info_markup = InlineKeyboardMarkup()
-credits_info_markup.add(InlineKeyboardButton("Кто мне должен?", callback_data="credits_to_user"))
-credits_info_markup.add(InlineKeyboardButton("Кому я должен?", callback_data="user_credits"))
-
-cancel_inline_button = InlineKeyboardButton("Отмена", callback_data="cancel")
-return_credit_inline_button = InlineKeyboardButton("Вернуть выбранные долги", callback_data="return_credit")
-cancel_credit_inline_button = InlineKeyboardButton("Я ничего еще не вернул", callback_data="credit_cancel")
+cancel_crt_credit_inline = InlineKeyboardButton("Отмена", callback_data=CALLBACK.cancel_crt_credit)
+return_credit_inline = InlineKeyboardButton("Вернуть выбранные долги", callback_data=CALLBACK.return_credits)
+cancel_return_credit_inline = InlineKeyboardButton("Отмена", callback_data=CALLBACK.cancel_return_credits)
 
 
 def get_check_markup(credit_id: Union[str, int], value: bool) -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup()
     if value:
-        markup.add(InlineKeyboardButton(f"Все верно{Emoji.TRUE}", callback_data="true"))
-        markup.add(InlineKeyboardButton(f"Это не так{Emoji.FALSE}", callback_data="false"))
-        markup.add(InlineKeyboardButton("Сохранить", callback_data=check_data.new(credit_id, "1")))
+        markup.add(InlineKeyboardButton(f"Все верно{Emoji.TRUE}", callback_data=CALLBACK.check_return_approve))
+        markup.add(InlineKeyboardButton(f"Это не так{Emoji.FALSE}", callback_data=CALLBACK.check_return_reject))
+        markup.add(InlineKeyboardButton("Сохранить", callback_data=check_return_data.new(credit_id, "1")))
     else:
-        markup.add(InlineKeyboardButton(f"Все верно{Emoji.FALSE}", callback_data="true"))
-        markup.add(InlineKeyboardButton(f"Это не так{Emoji.TRUE}", callback_data="false"))
-        markup.add(InlineKeyboardButton("Сохранить", callback_data=check_data.new(credit_id, "0")))
+        markup.add(InlineKeyboardButton(f"Все верно{Emoji.FALSE}", callback_data=CALLBACK.check_return_approve))
+        markup.add(InlineKeyboardButton(f"Это не так{Emoji.TRUE}", callback_data=CALLBACK.check_return_reject))
+        markup.add(InlineKeyboardButton("Сохранить", callback_data=check_return_data.new(credit_id, "0")))
     return markup
 
 
 def get_data_from_check(markup: InlineKeyboardMarkup) -> Tuple[int, str]:
     data = markup["inline_keyboard"][2][0].callback_data
-    parsed_data = check_data.parse(data)
+    parsed_data = check_return_data.parse(data)
     return int(parsed_data.get("credit_id")), parsed_data.get("value")
 
 
@@ -51,9 +52,9 @@ def get_credits_markup(user_credits: List[Credit], marked_credits: set) -> Inlin
         else:
             has_mark = 0
             text = f'{i} {Emoji.FALSE}'
-        markup.add(InlineKeyboardButton(text, callback_data=credit_data.new(credit.id, has_mark)))
-    markup.add(return_credit_inline_button)
-    markup.add(cancel_credit_inline_button)
+        markup.add(InlineKeyboardButton(text, callback_data=credit_choose_data.new(credit.id, has_mark)))
+    markup.add(return_credit_inline)
+    markup.add(cancel_return_credit_inline)
     return markup
 
 
@@ -61,15 +62,15 @@ def get_marked_credits(markup: InlineKeyboardMarkup) -> Set[int]:
     marked_credits = set()
     for _ in markup["inline_keyboard"]:
         for elem in _:
-            if "credit_chose" in elem.callback_data:
-                data = credit_data.parse(elem.callback_data)
+            if CALLBACK.choose_credit_for_return in elem.callback_data:
+                data = credit_choose_data.parse(elem.callback_data)
                 if data.get("has_mark") == "1":
                     marked_credits.add(int(data.get("index")))
     return marked_credits
 
 
 def get_credit_id(data: str) -> int:
-    return int(credit_data.parse(data).get("index"))
+    return int(credit_choose_data.parse(data).get("index"))
 
 
 def get_keyboard_users_for_credit(for_user_id: int, value: int, users: Set[int]) -> InlineKeyboardMarkup:
@@ -83,11 +84,11 @@ def get_keyboard_users_for_credit(for_user_id: int, value: int, users: Set[int])
                 has_mark = 0
                 text = f'{USERS[user_id]}{Emoji.FALSE}'
 
-            markup.add(InlineKeyboardButton(text, callback_data=user_data.new(user_id, has_mark)))
+            markup.add(InlineKeyboardButton(text, callback_data=user_choose_data.new(user_id, has_mark)))
     inline_save = InlineKeyboardButton("Сохранить", callback_data=credit_amount_data.new(value))
     markup.add(
         inline_save,
-        cancel_inline_button,
+        cancel_crt_credit_inline,
     )
     return markup
 
@@ -97,10 +98,10 @@ def get_data_from_markup(markup: InlineKeyboardMarkup) -> Tuple[int, Set[int]]:
     value = None
     for _ in markup["inline_keyboard"]:
         for elem in _:
-            if "save" in elem.callback_data:
+            if CALLBACK.save_credit in elem.callback_data:
                 value = int(credit_amount_data.parse(elem.callback_data).get("value"))
-            if "user" in elem.callback_data:
-                data = user_data.parse(elem.callback_data)
+            if CALLBACK.choose_user_for_credit in elem.callback_data:
+                data = user_choose_data.parse(elem.callback_data)
                 if data.get("has_mark") == "1":
                     users.add(int(data.get("id")))
     return value, users
@@ -109,9 +110,9 @@ def get_data_from_markup(markup: InlineKeyboardMarkup) -> Tuple[int, Set[int]]:
 def get_amount_from_markup(markup: InlineKeyboardMarkup) -> int:
     for _ in markup["inline_keyboard"]:
         for elem in _:
-            if "save" in elem.callback_data:
+            if CALLBACK.save_credit in elem.callback_data:
                 return int(credit_amount_data.parse(elem.callback_data).get("value"))
 
 
 def get_user_id(data: str) -> int:
-    return int(user_data.parse(data).get("id"))
+    return int(user_choose_data.parse(data).get("id"))
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import keyboards as kb
-import config
-from db_connector import DB
-from my_state import AddState
 from loguru import logger
 
-bot = Bot(token=config.TOKEN, proxy=config.PROXY)
+from my_state import AddState
+import keyboards as kb
+from keyboards import CALLBACK
+from db_connector import DB
+import config
+
+bot = Bot(token=config.TOKEN)  # Works fine without proxy (18.11.2020) , proxy=config.PROXY)
 dp = Dispatcher(bot, storage=MemoryStorage())
 db = DB()
 
@@ -21,159 +23,55 @@ def get_info(message: types.Message) -> str:
     return '' if len(msg) == 1 else msg[1]
 
 
-@dp.message_handler(check_user, text="+", state=None)
+@dp.message_handler(check_user, commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.answer("Привет!", reply_markup=kb.main_markup)
+
+
+@dp.message_handler(check_user, commands=['id'])
+async def get_id(message: types.Message):
+    await message.answer(f"{message.from_user.id}")
+
+
+# ======================================= ADD CREDIT =======================================
+@dp.message_handler(check_user, text="+")
 async def process_callback_plus(message: types.Message):
     await message.answer("Введите сумму(по желанию инфу по долгу через пробел)")
     await AddState.read_num.set()
 
 
-@dp.message_handler(check_user, text="info", state=None)
-async def process_callback_info(message: types.Message):
-    await message.answer("Что интересует?", reply_markup=kb.credits_info_markup)
-
-
-@dp.callback_query_handler(text_contains="user_credits")
-async def process_callback_user_credits(call: types.CallbackQuery):
-    user_credits = db.user_credits(call.from_user.id)
-    if not user_credits:
-        text = "Ты никому не должен. Свободен"
-        await call.message.edit_text(text)
-    else:
-        text = "Ты должен:"
-        for i, credit in enumerate(user_credits, 1):
-            text = f'{text}\n' \
-                   f'{i}) {credit.amount} руб. ему: {config.USERS[credit.to_id]}\n' \
-                   f'{credit.get_text_info_new_line()}' \
-                   f'Долг был добавлен {credit.get_date_str()}'
-        text += "Ты можешь выбрать долги, которые ты уже вернул:"
-        await call.message.edit_text(text, reply_markup=kb.get_credits_markup(user_credits, set()))
-    await call.answer()
-
-
-@dp.callback_query_handler(text_contains="credit_chose")
-async def process_callback_credit_chose(call: types.CallbackQuery):
-    marked_credits = kb.get_marked_credits(call.message.reply_markup)
-    credit_id = kb.get_credit_id(call.data)
-    user_id = int(call.from_user.id)
-
-    if credit_id in marked_credits:
-        marked_credits.remove(credit_id)
-    else:
-        marked_credits.add(credit_id)
-
-    await call.message.edit_reply_markup(
-        reply_markup=kb.get_credits_markup(db.user_credits(user_id), marked_credits))
-    await call.answer()
-
-
-@dp.callback_query_handler(text_contains="return_credit")
-async def process_callback_return_credit(call: types.CallbackQuery):
-    marked_credits = kb.get_marked_credits(call.message.reply_markup)
-
-    if not marked_credits:
-        text = "Ты ничего не отметил"
-    else:
-        returned_credits = []
-        text = "Ты вернул:"
-        for credit_id in marked_credits:
-            returned_credits.append(credit_id)
-            credit = db.get_credit(credit_id)
-            text = f'{credit.amount} руб. ему: {config.USERS[credit.to_id]}\n' \
-                   f'{credit.get_text_info_new_line()}' \
-
-        db.return_credit(returned_credits)
-        await call.message.edit_text(text)
-
-    await call.answer(text=text, show_alert=True)
-
-    for credit_id in marked_credits:
-        credit = db.get_credit(credit_id)
-        markup = kb.get_check_markup(credit_id, True)
-        message = f'Тебе {config.USERS[credit.from_id]} вернул {credit.amount} руб.\n' \
-                  f'{credit.get_text_info_new_line()}'
-        try:
-            await bot.send_message(credit.to_id, message, reply_markup=markup)
-        except Exception:
-            pass
-
-
-@dp.callback_query_handler(text_contains="true")
-async def process_callback_check_true(call: types.CallbackQuery):
-    credit_id, value = kb.get_data_from_check(call.message.reply_markup)
-    if value == "0":
-        await call.message.edit_reply_markup(kb.get_check_markup(credit_id, True))
-    await call.answer()
-
-
-@dp.callback_query_handler(text_contains="false")
-async def process_callback_check_false(call: types.CallbackQuery):
-    credit_id, value = kb.get_data_from_check(call.message.reply_markup)
-    if value == "1":
-        await call.message.edit_reply_markup(kb.get_check_markup(credit_id, False))
-    await call.answer()
-
-
-@dp.callback_query_handler(text_contains="check")
-async def process_callback_check(call: types.CallbackQuery):
-    credit_id, value = kb.get_data_from_check(call.message.reply_markup)
-
-    await call.answer()
-
-    if value == "1":
-        await call.message.delete_reply_markup()
-    else:
-        db.reject_return_credit(credit_id)
-        text = call.message.text
-        await call.message.edit_text(text + "\nОтмена")
-        credit = db.get_credit(credit_id)
-        message = f"{config.USERS[credit.to_id]} отметил, что ты не вернул {credit.amount} руб.\n" \
-                  f"{credit.get_text_info_new_line()}" \
-
-        await bot.send_message(credit.from_id, message)
-
-
-@dp.callback_query_handler(text_contains="credit_cancel")
-async def process_callback_credit_cancel(call: types.CallbackQuery):
-    text = '\n'.join(call.message.text.split("\n")[:-1])
-    await call.message.edit_text(text)
-    await call.answer()
-
-
-@dp.callback_query_handler(text_contains="credits_to_user")
-async def process_callback_credits_to_user(call: types.CallbackQuery):
-    credits_to_user = db.credits_to_user(call.from_user.id)
-    if not credits_to_user:
-        text = "Тебе никто не должен. Можешь спать спокойно"
-    else:
-        text = "Тебе должны:"
-        for i, credit in enumerate(credits_to_user, 1):
-            text = f'{text}\n' \
-                   f'{i}) {config.USERS[credit.from_id]}: {credit.amount} руб.\n' \
-                   f'{credit.get_text_info_new_line()}' \
-                   f'Долг был добавлен {credit.get_date_str()}'
-
-    await call.message.edit_text(text)
-    await call.answer()
-
-
 @dp.message_handler(state=AddState.read_num)
 async def read_num_from_user(message: types.Message, state: FSMContext):
+    await state.finish()
+
     message_text = message.text.split(" ")
     try:
         value = int(message_text[0])
     except ValueError:
         await message.answer("Требуется число в начале сообщения")
-        await state.finish()
         return
 
     text = f"Кто тебе должен {value} руб?\n" \
            f"{' '.join(message_text[1:])}"
 
     await message.answer(text, reply_markup=kb.get_keyboard_users_for_credit(message.from_user.id, value, set()))
-    await state.finish()
 
 
-@dp.callback_query_handler(text_contains="save")
+@dp.callback_query_handler(text_contains=CALLBACK.choose_user_for_credit)
+async def process_callback(call: types.CallbackQuery):
+    value, users = kb.get_data_from_markup(call.message.reply_markup)
+    user_id = kb.get_user_id(call.data)
+
+    if user_id in users:
+        users.remove(user_id)
+    else:
+        users.add(user_id)
+
+    await call.message.edit_reply_markup(reply_markup=kb.get_keyboard_users_for_credit(call.from_user.id, value, users))
+    await call.answer()
+
+
+@dp.callback_query_handler(text_contains=CALLBACK.save_credit)
 async def process_callback_save(call: types.CallbackQuery):
     value, users = kb.get_data_from_markup(call.message.reply_markup)
     if not users:
@@ -199,7 +97,7 @@ async def process_callback_save(call: types.CallbackQuery):
     await call.answer(text=text, show_alert=True)
 
 
-@dp.callback_query_handler(text_contains="cancel")
+@dp.callback_query_handler(text_contains=CALLBACK.cancel_crt_credit)
 async def process_callback_cancel(call: types.CallbackQuery):
     value = kb.get_amount_from_markup(call.message.reply_markup)
     await call.message.edit_text(
@@ -209,28 +107,129 @@ async def process_callback_cancel(call: types.CallbackQuery):
     await call.answer(text="Отмена")
 
 
-@dp.callback_query_handler(text_contains="user")
-async def process_callback(call: types.CallbackQuery):
-    value, users = kb.get_data_from_markup(call.message.reply_markup)
-    user_id = kb.get_user_id(call.data)
-
-    if user_id in users:
-        users.remove(user_id)
+# ======================================= REMOVE CREDIT =======================================
+@dp.message_handler(check_user, text='-')
+async def process_callback_user_credits(message: types.Message):
+    user_credits = db.user_credits(message.from_user.id)
+    if not user_credits:
+        text = "Ты никому не должен. Свободен"
+        await message.answer(text)
     else:
-        users.add(user_id)
+        text = "Ты должен:"
+        for i, credit in enumerate(user_credits, 1):
+            text = f'{text}\n' \
+                   f'{i}) {credit.amount} руб. ему: {config.USERS[credit.to_id]}\n' \
+                   f'{credit.get_text_info_new_line()}' \
+                   f'Долг был добавлен {credit.get_date_str()}'
+        text += "Ты можешь выбрать долги, которые ты уже вернул:"
+        await message.answer(text, reply_markup=kb.get_credits_markup(user_credits, set()))
 
-    await call.message.edit_reply_markup(reply_markup=kb.get_keyboard_users_for_credit(call.from_user.id, value, users))
+
+@dp.callback_query_handler(text_contains=CALLBACK.choose_credit_for_return)
+async def process_callback_credit_chose(call: types.CallbackQuery):
+    marked_credits = kb.get_marked_credits(call.message.reply_markup)
+    credit_id = kb.get_credit_id(call.data)
+    user_id = int(call.from_user.id)
+
+    if credit_id in marked_credits:
+        marked_credits.remove(credit_id)
+    else:
+        marked_credits.add(credit_id)
+
+    await call.message.edit_reply_markup(
+        reply_markup=kb.get_credits_markup(db.user_credits(user_id), marked_credits))
     await call.answer()
 
 
-@dp.message_handler(check_user, commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.answer("Привет!", reply_markup=kb.main_markup)
+@dp.callback_query_handler(text_contains=CALLBACK.return_credits)
+async def process_callback_return_credit(call: types.CallbackQuery):
+    marked_credits = kb.get_marked_credits(call.message.reply_markup)
+
+    if not marked_credits:
+        text = "Ты ничего не отметил"
+    else:
+        returned_credits = []
+        text = "Ты вернул:"
+        for credit_id in marked_credits:
+            returned_credits.append(credit_id)
+            credit = db.get_credit(credit_id)
+            text = f'{credit.amount} руб. ему: {config.USERS[credit.to_id]}\n' \
+                   f'{credit.get_text_info_new_line()}'
+
+        db.return_credit(returned_credits)
+        await call.message.edit_text(text)
+
+    await call.answer(text=text, show_alert=True)
+
+    for credit_id in marked_credits:
+        credit = db.get_credit(credit_id)
+        markup = kb.get_check_markup(credit_id, True)
+        message = f'Тебе {config.USERS[credit.from_id]} вернул {credit.amount} руб.\n' \
+                  f'{credit.get_text_info_new_line()}'
+        try:
+            await bot.send_message(credit.to_id, message, reply_markup=markup)
+        except Exception:
+            pass
 
 
-@dp.message_handler(check_user, commands=['id'])
-async def get_id(message: types.Message):
-    await message.answer(f"{message.from_user.id}")
+@dp.callback_query_handler(text_contains=CALLBACK.cancel_return_credits)
+async def process_callback_credit_cancel(call: types.CallbackQuery):
+    text = '\n'.join(call.message.text.split("\n")[:-1])
+    await call.message.edit_text(text)
+    await call.answer()
+
+
+@dp.callback_query_handler(text_contains=CALLBACK.check_return_approve)
+async def process_callback_check_true(call: types.CallbackQuery):
+    credit_id, value = kb.get_data_from_check(call.message.reply_markup)
+    if value == "0":
+        await call.message.edit_reply_markup(kb.get_check_markup(credit_id, True))
+    await call.answer()
+
+
+@dp.callback_query_handler(text_contains=CALLBACK.check_return_reject)
+async def process_callback_check_false(call: types.CallbackQuery):
+    credit_id, value = kb.get_data_from_check(call.message.reply_markup)
+    if value == "1":
+        await call.message.edit_reply_markup(kb.get_check_markup(credit_id, False))
+    await call.answer()
+
+
+@dp.callback_query_handler(text_contains=CALLBACK.check_return_of_credit)
+async def process_callback_check(call: types.CallbackQuery):
+    credit_id, value = kb.get_data_from_check(call.message.reply_markup)
+
+    await call.answer()
+
+    if value == "1":
+        await call.message.delete_reply_markup()
+    else:
+        db.reject_return_credit(credit_id)
+        text = call.message.text
+        await call.message.edit_text(text + "\nОтмена")
+        credit = db.get_credit(credit_id)
+        message = f"{config.USERS[credit.to_id]} отметил, что ты не вернул {credit.amount} руб.\n" \
+                  f"{credit.get_text_info_new_line()}"
+
+        await bot.send_message(credit.from_id, message)
+
+
+# ======================================= INFO =======================================
+@dp.message_handler(check_user, text='info')
+async def process_callback_credits_to_user(message: types.Message):
+    credits_to_user = db.credits_to_user(message.from_user.id)
+
+    if not credits_to_user:
+        text = "Тебе никто не должен. Можешь спать спокойно"
+    else:
+        text = "Тебе должны:"
+        for i, credit in enumerate(credits_to_user, 1):
+            text = f'{text}\n' \
+                   f'{i}) {config.USERS[credit.from_id]}: {credit.amount} руб.\n' \
+                   f'{credit.get_text_info_new_line()}' \
+                   f'Долг был добавлен {credit.get_date_str()}'
+
+    await message.answer(text, reply_markup=kb.main_markup)
 
 
 @logger.catch()
