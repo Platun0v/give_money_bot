8ba7f918dec84e87342442f8277fe27d45c605cb
Gooxezecyra <nikitaSergeev692@gmail.com>
2022-03-11 18:21:37 +0300

    parser update

--- additional_stuff/parser.hs
+++ additional_stuff/parser.hs
@@ -1,67 +1,61 @@
 module Main where
-import Data.Char
+import           Control.Exception
+import           Data.Char                      ( isNumber )
 
 firstOrderOperations :: [Char]
 firstOrderOperations = ['/', '*']
 secondOrderOperations :: [Char]
 secondOrderOperations = ['-', '+']
-digits :: [Char]
-digits = ['1', '2', '3', '4', '5', '6','7','8','9','0']
-
-
 
 strToRPN :: String -> [String] -> [String] -> [String]
 strToRPN xss stack out
-    | null xss =                       out ++ stack
-    | x == '(' =                       strToRPN xs ("(" : stack) out
-    | x == ')' && null afterBracket = error "parsing poshel po pizde"
-    | x == ')' =                       strToRPN xs (tail afterBracket) (out ++ beforeBracket)
-    | x == ' ' =                       strToRPN xs stack out
-    | x `elem` firstOrderOperations =  strToRPN xs ([x] : afterNotFirstPriority) (out ++ whileFirstPriority)
-    | x `elem` secondOrderOperations = strToRPN xs ([x] : afterBracket) (out ++ beforeBracket)
-    | x `elem` digits =               strToRPN afterNumber stack (out ++ [number])
-    | otherwise = error "parse error"
-    where   beforeBracket =          takeWhile (/= "(") stack
-            afterBracket =           dropWhile (/= "(") stack
-            whileFirstPriority =     takeWhile (\y -> head y `elem` firstOrderOperations) stack
-            afterNotFirstPriority =  dropWhile (\y -> head y `elem` firstOrderOperations) stack
-            number =                 takeWhile (\y -> y `elem` digits || y == '.') xss
-            afterNumber =            dropWhile (\y -> y `elem` digits || y == '.') xss
-            x =                      head xss
-            xs =                     tail xss
+  | null xss = out ++ stack
+  | x == '(' = strToRPN xs ("(" : stack) out
+  | x == ')' && null afterBracket = error "parsing poshel po pizde"
+  | x == ')' = strToRPN xs (tail afterBracket) (out ++ beforeBracket)
+  | x == ' ' = strToRPN xs stack out
+  | x `elem` firstOrderOperations = strToRPN xs
+                                             ([x] : afterNotFirstPriority)
+                                             (out ++ whileFirstPriority)
+  | x `elem` secondOrderOperations = strToRPN xs
+                                              ([x] : afterBracket)
+                                              (out ++ beforeBracket)
+  | isNumber x = strToRPN afterNumber stack (out ++ [number])
+  | otherwise = error "parse error"
+ where
+  beforeBracket = takeWhile (/= "(") stack
+  afterBracket  = dropWhile (/= "(") stack
+  whileFirstPriority =
+    takeWhile (\y -> head y `elem` firstOrderOperations) stack
+  afterNotFirstPriority =
+    dropWhile (\y -> head y `elem` firstOrderOperations) stack
+  number      = takeWhile (\y -> isNumber y || y == '.') xss
+  afterNumber = dropWhile (\y -> isNumber y || y == '.') xss
+  x           = head xss
+  xs          = tail xss
 
-test :: String -> [String]
-test str = strToRPN str [] []
+rpn :: String -> [String]
+rpn str = strToRPN str [] []
 
-eval :: [String] -> [String] -> Float
-eval before next
-    | null next && length before == 1 = read $ head before :: Float
-    | not (all (\x -> isDigit x || x == '.' || x == '-') $ head next) && length before < 2 = error "eblan?"
-    | head next == "+" =                eval (newBefore ++ [show $ first + second]) (tail next)
-    | head next == "-" =                eval (newBefore ++ [show $ first - second]) (tail next)
-    | head next == "*" =                eval (newBefore ++ [show $ first * second]) (tail next)
-    | head next == "/" =                eval (newBefore ++ [show $ first / second]) (tail next)
-    | all (\x -> isDigit x || x == '.' || x == '-') (head next) =         eval (before ++ [head next]) (tail next)
-    | otherwise =                       error "unexprected eblan"
-    where second =     read $ last before :: Float
-          first =      read $ head $ tail (reverse before) :: Float
-          newBefore =  reverse(tail $ tail (reverse before))
+transform :: [String] -> String
+transform (elem : list) = elem ++ " " ++ transform list
+transform []            = ""
 
-evalFirstBrackets :: String -> [String]
-evalFirstBrackets str = ('-' : head result) : tail result
-                where brackets = tail $ dropWhile (/= '(') $ takeWhile (/= ')') str
-                      afterBrackets = tail $ dropWhile (/= ')') str
-                      result = test $ show(eval [] (test brackets)) ++ afterBrackets
+solveRPN :: String -> [Double]
+solveRPN = foldl foldingFunction [] . words
+ where
+  foldingFunction (x : y : ys) "*"          = (x * y) : ys
+  foldingFunction (x : y : ys) "+"          = (x + y) : ys
+  foldingFunction (x : y : ys) "-"          = (y - x) : ys
+  foldingFunction (x     : xs) "-"          = -x : xs
+  foldingFunction xs           numberString = read numberString : xs
 
 
-compute :: String -> Int
-compute string |head string == '-' && isDigit (head $ tail string) = round $ eval [] modified
-               |head string == '-' && head (tail string) == '(' = round $ eval [] (evalFirstBrackets string)
-               |otherwise = round $ eval [] (test string)
-               where list = test (tail string)
-                     modified = ('-' : head list) : tail list
 
-main :: IO ()
 main = do
-    str <- getLine
-    print $ compute str
+  str <- getLine
+  let result = solveRPN $ transform $ rpn str
+  if length result > 1 
+    then print "dolabeb" 
+    else print $ head result
+
