aa7bb6f3f9f6e40c2663b1397725d197c0dfd5f5
Platun0v <me@platun0v.ru>
2022-05-18 00:41:42 +0300

    Add additional fields to user model

--- give_money_bot/db/models.py
+++ give_money_bot/db/models.py
@@ -37,18 +37,18 @@ class Credit(Base):
     returned = sqlalchemy.Column(sqlalchemy.Boolean, nullable=False, default=False)
     return_date = sqlalchemy.Column(sqlalchemy.DateTime)
 
-    def get_date_str(self) -> str:
-        return f"{self.date.day}.{self.date.month}.{self.date.year}"
-
-    def get_return_date_str(self) -> str:
-        if self.return_date:
-            return f"{self.return_date.day}.{self.return_date.month}.{self.return_date.year}"
-        return ""
-
-    def get_text_info_new_line(self) -> str:
-        if self.text_info:
-            return f"{self.text_info}\n"
-        return ""
+    # def get_date_str(self) -> str:
+    #     return f"{self.date.day}.{self.date.month}.{self.date.year}"
+    #
+    # def get_return_date_str(self) -> str:
+    #     if self.return_date:
+    #         return f"{self.return_date.day}.{self.return_date.month}.{self.return_date.year}"
+    #     return ""
+    #
+    # def get_text_info_new_line(self) -> str:
+    #     if self.text_info:
+    #         return f"{self.text_info}\n"
+    #     return ""
 
     def get_amount(self) -> int:
         return self.amount - (0 if self.discount is None else self.discount)
@@ -65,24 +65,10 @@ class User(Base):
     user_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=False, unique=True, primary_key=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     admin = sqlalchemy.Column(sqlalchemy.Boolean, nullable=False, default=False)
-    # show_users = sqlalchemy.Column(sqlalchemy.String, nullable=True, default="")
-    # credits = relationship('Credit', primaryjoin="or_(users.user_id==credits.creditor_id, users.user_id==credits.debtor_id)", lazy='dynamic')
-
-    # def get_show_users(self) -> Set[int]:
-    #     if self.show_users is None or len(self.show_users) == 0:
-    #         return set()
-    #     return set(map(int, self.show_users.split(",")))
-    #
-    # def add_show_user(self, user: Union[int, "User"]) -> None:
-    #     if self.show_users is None:
-    #         self.show_users = ""
-    #
-    #     res = self.get_show_users()
-    #     if isinstance(user, User):
-    #         res.add(user.user_id)
-    #     else:
-    #         res.add(user)
-    #     self.show_users = ",".join(map(str, res))
+    phone_number = sqlalchemy.Column(sqlalchemy.String, nullable=False, default="")
+    note = sqlalchemy.Column(sqlalchemy.String, nullable=False, default="")
+    tg_alias = sqlalchemy.Column(sqlalchemy.String, nullable=False, default="")
+    tg_name = sqlalchemy.Column(sqlalchemy.String, nullable=False, default="")
 
     def __repr__(self) -> str:
         return f"<User(id='{self.user_id}', name='{self.name}', admin='{self.admin}')>"
