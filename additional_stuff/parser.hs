250a46617f8480144989e4cf6257ea20decfd356
Gooxezecyra <nikitasergeev692@gmail.com>
2022-07-21 21:32:57 +0300

    new parser

--- additional_stuff/parser.hs
+++ additional_stuff/parser.hs
@@ -1,147 +1,59 @@
-{-# LANGUAGE LambdaCase #-}
 
--- module Parsing where
-
-import Control.Applicative ((<|>))
-import Control.Monad (liftM, liftM2)
-import Data.Complex
 import qualified Data.Map as M
-import qualified Text.ParserCombinators.Parsec as P
-import qualified Text.ParserCombinators.Parsec.Expr as P
-import GHC.Base (VecElem(Int16ElemRep))
-import Data.Map (valid)
-import Data.Maybe (fromMaybe)
+import Lex
+import Synt
 
-
-import Foreign.C.Types
-import Foreign.C.String
-
-
--- | Mathematical expressions.
-data Expr a
-  = Num a
-  | Var String
-  | Neg (Expr a)
-  | Add (Expr a) (Expr a)
-  | Sub (Expr a) (Expr a)
-  | Mul (Expr a) (Expr a)
-  | Div (Expr a) (Expr a)
-  | Pow (Expr a) (Expr a)
-  | Sqrt (Expr a)
-  | Exp (Expr a)
-  | Log (Expr a)
-  | Abs (Expr a)
-  | Sin (Expr a)
-  | Cos (Expr a)
-  | Tan (Expr a)
+newtype Context =
+  Context
+    { getContext :: M.Map String Int
+    }
   deriving (Show)
 
--- | Parse a mathematical expression.
---
--- >>> parse "exp(-pi*i)+1"
--- Right (Add (Exp (Mul (Neg (Var "pi")) (Var "i"))) (Num 1.0))
-parse :: Floating a => String -> Either P.ParseError (Expr a)
-parse = P.parse build "" . (:) '(' . flip (++) ")" . filter (/= ' ')
+pull :: Maybe a -> a
+pull (Just m) = m
+pull Nothing = error "Undefined variable"
 
-build :: Floating a => P.Parser (Expr a)
-build = P.buildExpressionParser table factor
+createContext :: Context
+createContext = Context {getContext = M.empty}
 
-table :: Floating a => [[P.Operator Char st (Expr a)]]
-table =
-  [ [prefix "sin" Sin]
-  , [prefix "cos" Cos]
-  , [prefix "tan" Tan]
-  , [prefix "abs" Abs]
-  , [prefix "exp" Exp]
-  , [prefix "sqrt" Sqrt]
-  , [prefix "log" Log]
-  , [binary "^" Pow P.AssocRight]
-  , [prefix "-" Neg]
-  , [binary "*" Mul P.AssocLeft, binary "/" Div P.AssocLeft]
-  , [binary "+" Add P.AssocLeft, binary "-" Sub P.AssocLeft]
-  ]
-  where
-    binary s f a = P.Infix (P.string s >> return f) a
-    prefix s f = P.Prefix (P.try (P.string s) >> return f)
+getValue :: Context -> String -> Maybe Int
+getValue ctx name = M.lookup name $ getContext ctx
 
-factor :: Floating a => P.Parser (Expr a)
-factor =
-  do _ <- P.char '('
-     expr <- build
-     _ <- P.char ')'
-     return $! expr
-     <|> variable
+solveExp :: Context -> Exp -> Maybe Int
+solveExp ctx exp =
+  case exp of
+    (Let name expl rexp) -> solveExp newCtx rexp
+      where newCtx =
+              Context
+                { getContext =
+                    M.insert name (pull (solveExp ctx expl)) (getContext ctx)
+                }
+    (Exp1 exp1) -> solveExp1 ctx exp1
 
-variable :: Floating a => P.Parser (Expr a)
-variable =
-  do var <- P.many1 P.letter
-     return $! Var var
-     <|> number
+solveExp1 :: Context -> Exp1 -> Maybe Int
+solveExp1 ctx exp1 =
+  case exp1 of
+    (Plus lexp1 rterm) -> (+) <$> solveExp1 ctx lexp1 <*> solveTerm ctx rterm
+    (Minus lexp1 rterm) -> (-) <$> solveExp1 ctx lexp1 <*> solveTerm ctx rterm
+    (Term term) -> solveTerm ctx term
+    (Neg term) -> negate <$> solveTerm ctx term
 
-number :: Floating a => P.Parser (Expr a)
-number = do
-  pr <- P.many1 P.digit
-  let n = foldl stl 0 pr
-  P.option (Num n) . P.try $ do
-    _ <- P.char '.'
-    su <- P.many1 P.digit
-    return $! Num $ n + foldr str 0 su
-  where
-    stl a x = ctn x - ctn '0' + a * 10
-    str x a = (ctn x - ctn '0' + a) / 10
-    ctn = realToFrac . fromEnum
+solveTerm :: Context -> Term -> Maybe Int
+solveTerm ctx term =
+  case term of
+    (Mul lterm rfactor) ->
+      (*) <$> solveTerm ctx lterm <*> solveFactor ctx rfactor
+    (Div lterm rfactor) ->
+      div <$> solveTerm ctx lterm <*> solveFactor ctx rfactor
+    (Factor factor) -> solveFactor ctx factor
 
--- | Evaluate a mathematical expression
---   using the supplied variable definitions.
---
--- > >>> :m + Data.Complex Data.Map
--- > >>> let def = fromList [("pi", pi), ("i", 0 :+ 1)]
--- > >>> evaluate def . Just $ Add (Exp (Mul (Neg (Var "pi")) (Var "i"))) (Num 1.0)
--- > Just (0.0 :+ (-1.2246467991473532e-16))
---
-evaluate ::
-     Floating a
-  => M.Map String a -- ^ Variable definitions.
-  -> Maybe (Expr a) -- ^ Mathematical expression.
-  -> Maybe a
-evaluate def =
-  \case
-    Just (Num num) -> Just num
-    Just (Var var) -> M.lookup var def
-    Just (Neg e1) -> fmap negate (evaluate def $ Just e1)
-    Just (Add e1 e2) ->
-      liftM2 (+) (evaluate def $ Just e1) (evaluate def $ Just e2)
-    Just (Sub e1 e2) ->
-      liftM2 (-) (evaluate def $ Just e1) (evaluate def $ Just e2)
-    Just (Mul e1 e2) ->
-      liftM2 (*) (evaluate def $ Just e1) (evaluate def $ Just e2)
-    Just (Div e1 e2) ->
-      liftM2 (/) (evaluate def $ Just e1) (evaluate def $ Just e2)
-    Just (Pow e1 e2) ->
-      liftM2 (**) (evaluate def $ Just e1) (evaluate def $ Just e2)
-    Just (Sqrt e1) -> fmap (** 0.5) (evaluate def $ Just e1)
-    Just (Exp e1) -> fmap exp (evaluate def $ Just e1)
-    Just (Log e1) -> fmap log (evaluate def $ Just e1)
-    Just (Abs e1) -> fmap abs (evaluate def $ Just e1)
-    Just (Sin e1) -> fmap sin (evaluate def $ Just e1)
-    Just (Cos e1) -> fmap cos (evaluate def $ Just e1)
-    Just (Tan e1) -> fmap tan (evaluate def $ Just e1)
-    _ -> Nothing
-  where
-    inv = (/) 1
-
-
-exec :: Floating a => Either P.ParseError (Expr a) -> Maybe (Expr a)
-exec expression = case expression of
-  (Right val) -> Just val
-  (Left err) -> error (show err)
-
+solveFactor :: Context -> Factor -> Maybe Int
+solveFactor ctx factor =
+  case factor of
+    (Num n) -> Just n
+    (Var s) -> getValue ctx s
+    (Brack exp) -> solveExp ctx exp
 
 main = do
-  expression <- getLine
-  let maybeExpr = exec $ parse expression
-  let d = M.empty :: M.Map String a
-  let maybeResult =  evaluate d maybeExpr
-  case maybeResult of
-    (Just value) -> if value == 1/0 then error "useless line\nДеление на 0" else print value
-    Nothing -> error "useless line\nНеправильное выражение"
+  s <- getLine
+  mapM (print . pull . solveExp createContext . synt . alexScanTokens) (lines s)
